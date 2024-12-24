f880145f01146a615abd77e1544b79c41386b969
caparniyazi <capar.niyazi.tr@gmail.com>
2024-12-24 15:12:46 +0300

    More lambda expressions.

--- src/main/java/com/caparniyazi/ds/lambda/LambdaUtil.java
+++ src/main/java/com/caparniyazi/ds/lambda/LambdaUtil.java
@@ -1,11 +1,32 @@
 package com.caparniyazi.ds.lambda;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class LambdaUtil {
+    // Data fields
+    /*
+    lambda expression that takes a double argument (x) and an integer argument (n).
+    The method result is the double value raised to the power n. Doing this using a Java
+    API method and also using a loop.
+     */
+    static BiFunction<Double, Integer, Double> power = Math::pow;
+    static BiFunction<Double, Integer, Double> power2 = (x, n) -> {
+        double result = 1.0;
+
+        for (int i = 0; i < n; i++) {
+            result *= x;
+        }
+
+        return result;
+    };
+
     public static void main(String[] args) {
         Function<Integer, Double> f = angle -> Math.cos(Math.toRadians(angle));
+
         show(0, 360, 30, f);
+        System.out.println(power.apply(3.0, 3));
+        System.out.println(power2.apply(3.0, 3));
     }
 
     /**
