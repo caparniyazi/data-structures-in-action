5af3b682eb1be252aebc8c54ec730a4f7ba38a2e
caparniyazi <capar.niyazi.tr@gmail.com>
2025-02-05 10:50:09 +0300

    TypeScript modules.

---
+++ src/main/typescript-training/04-classes/03-modules/Driver.ts
@@ -0,0 +1,5 @@
+import {Customer} from "./Customer"; // Import Customer class from the Customer file.
+
+let myCustomer = new Customer("Niyazi", "Çapar");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
