ea367fcd8e8b05ae20ca30d480608269e8574b96
caparniyazi <capar.niyazi.tr@gmail.com>
2025-02-07 21:21:56 +0300

    TypeScript collections.

---
+++ src/main/typescript-training/09-collections/ArrayDriver.ts
@@ -0,0 +1,6 @@
+let list: number[] = [1, 2, 3];
+
+// Using generic array syntax
+let genList: Array<number> = [1, 2, 3];
+
+console.log(genList);
---
+++ src/main/typescript-training/09-collections/Tuples.ts
@@ -0,0 +1,2 @@
+let person1: [string, number] = ["Niyazi", 34];
+console.log(person1);
