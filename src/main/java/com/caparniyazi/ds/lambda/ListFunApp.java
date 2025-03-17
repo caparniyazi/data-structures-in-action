00bd65160c8b309b96b94378a4badb25ed7da01a
caparniyazi <capar.niyazi.tr@gmail.com>
2025-03-17 21:15:55 +0300

    Concatenation functional way.

--- src/main/java/com/caparniyazi/ds/lambda/ListFunApp.java
+++ src/main/java/com/caparniyazi/ds/lambda/ListFunApp.java
@@ -2,6 +2,8 @@ package com.caparniyazi.ds.lambda;
 
 import java.util.List;
 
+import static com.caparniyazi.ds.lambda.ListFun.concat;
+
 public class ListFunApp {
     public static void main(String[] args) {
         System.out.println("Our list:");
@@ -24,7 +26,12 @@ public class ListFunApp {
         reversedList.forEach(System.out::println);
         System.out.println("----------------");
 
-        System.out.println("Functional list out of collection:");
+        System.out.println("Concatenating list:");
+        ListFun<Integer> list1 = ListFun.list(3, 6, 9);
+        ListFun<Integer> list2 =  ListFun.list(34, 44, 23);
+        concat(list1, list2).forEach(System.out::println);
+
+        System.out.println("Constructing functional list out of collection:");
         List<Integer> result = List.of(1, 3, 5, 7);
         list.addAll(result).forEach(System.out::println);
     }
