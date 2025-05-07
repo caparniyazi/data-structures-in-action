d6e439c37f10c77e5a0b0b7f8b74a9059fc73cb4
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-07 15:11:05 +0300

    AI agents.

---
+++ src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentObject.java
@@ -0,0 +1,11 @@
+package com.caparniyazi.ds.ai.agent;
+
+/**
+ * An interface used to indicate any object that can belong within an
+ * Environment.
+ *
+ * @author Ravi Mohan
+ * @author Ciaran O'Reilly
+ */
+public interface EnvironmentObject {
+}
