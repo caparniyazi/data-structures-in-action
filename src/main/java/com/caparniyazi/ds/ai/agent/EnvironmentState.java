e3f7091954cb905ec35c177de6fa27d7b78db1a0
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-08 11:14:47 +0300

    AI EnvironmentState

--- src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
+++ src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
@@ -9,6 +9,10 @@ package com.caparniyazi.ds.ai.agent;
  * If the sequence is desirable, then the agent has performed well.
  * This notion of desirability is captured by a performance measure that
  * evaluates any given sequence of environment states.
+ * <p/>
+ * Notice that we said environment states, not agent states.
+ * If we define success in terms of agent's opinion of its own performance,
+ * an agent could achieve perfect rationality simply by deluding itself that its performance was perfect.
  *
  * @author Ciaran O'Reilly
  */
