c0dcee72e532db4f1da234a895edf64728fd4269
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-08 11:10:31 +0300

    AI EnvironmentState

--- src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
+++ src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
@@ -2,6 +2,13 @@ package com.caparniyazi.ds.ai.agent;
 
 /**
  * An interface used to indicate a possible state of an Environment.
+ * <p/>
+ * When an agent is plunked down in an environment, it generates a
+ * sequence of actions according to the percepts it receives.
+ * This sequence of actions causes the environment to go through a sequence of states.
+ * If the sequence is desirable, then the agent has performed well.
+ * This notion of desirability is captured by a performance measure that
+ * evaluates any given sequence of environment states.
  *
  * @author Ciaran O'Reilly
  */
