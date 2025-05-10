ccdf4a1be4ef8adb96a6b4a1e1c4a4e29d4ec422
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-10 13:04:17 +0300

    AI EnvironmentState.

--- src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
+++ src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
@@ -16,6 +16,24 @@ package com.caparniyazi.ds.ai.agent;
  * <p/>
  * As a general rule, it is better to design performance measures according to what one actually
  * wants in the environment, rather than according to how one thinks the agent should behave.
+ * <p/>
+ * If the next state of the environment is completely determined by the current state
+ * and the action executed by the agent, then we say the environment
+ * is deterministic; otherwise, it is stochastic.
+ * Taxi driving is clearly stochastic, because one can never predict the behavior of traffic exactly;
+ * moreover, one’s tires blow out, and one’s engine seizes up without warning.
+ * <p/>
+ * We say the environment is uncertain if it is not fully observable or not deterministic.
+ * <p/>
+ * In an episodic task environment, the agent’s experience is
+ * divided into atomic episodes.
+ * In each episode, the agent receives a percept and then performs a single action.
+ * Crucially, the next episode does not depend on the actions taken in previous episodes.
+ * In sequential environments, on the other hand, the current decision could affect
+ * all future decisions.
+ * Chess and taxi driving are sequential.
+ * Episodic environments are much simpler than sequential
+ * environments because the agent does not need to think ahead.
  *
  * @author Ciaran O'Reilly
  */
