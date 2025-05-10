07a34f3a3b38b0e707b20cea89d922ba4d296f7b
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-10 19:07:18 +0300

    AI - Properties of a number of familiar environments.

--- src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
+++ src/main/java/com/caparniyazi/ds/ai/agent/EnvironmentState.java
@@ -21,11 +21,11 @@ package com.caparniyazi.ds.ai.agent;
  * and the action executed by the agent, then we say the environment
  * is deterministic; otherwise, it is stochastic.
  * Taxi driving is clearly stochastic, because one can never predict the behavior of traffic exactly;
- * moreover, one’s tires blow out, and one’s engine seizes up without warning.
+ * moreover, one&rsquo;s tires blow out, and one&rsquo;s engine seizes up without warning.
  * <p/>
  * We say the environment is uncertain if it is not fully observable or not deterministic.
  * <p/>
- * In an episodic task environment, the agent’s experience is
+ * In an episodic task environment, the agent&rsquo;s experience is
  * divided into atomic episodes.
  * In each episode, the agent receives a percept and then performs a single action.
  * Crucially, the next episode does not depend on the actions taken in previous episodes.
@@ -34,6 +34,100 @@ package com.caparniyazi.ds.ai.agent;
  * Chess and taxi driving are sequential.
  * Episodic environments are much simpler than sequential
  * environments because the agent does not need to think ahead.
+ * <p/>
+ * If the environment can change while an agent is deliberating, then
+ * we say the environment is dynamic for that agent; otherwise, it is static.
+ * If the environment itself does not change with the
+ * passage of time but the agent&rsquo;s performance score does, then we say the environment is semi dynamic.
+ * Taxi driving is clearly dynamic: the other cars and the taxi itself keep moving
+ * while the driving algorithm dithers about what to do next.
+ * Chess, when played with a clock, is semi-dynamic.
+ * Crossword puzzles are static.
+ * <p/>
+ * The hardest case is partially observable, multi-agent, stochastic,
+ * sequential, dynamic, continuous, and unknown.
+ * <p/>
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Crossword puzzle
+ * Observable: Fully.
+ * Agents: Single.
+ * Deterministic: Deterministic.
+ * Episodic: Episodic.
+ * Static: Static.
+ * Discrete: Discrete.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Chess with a clock.
+ * Observable: Fully.
+ * Agents: Multi.
+ * Deterministic: Deterministic.
+ * Episodic: Sequential.
+ * Static: Semi.
+ * Discrete: Discrete.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Backgammon.
+ * Observable: Fully.
+ * Agents: Multi.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Static.
+ * Discrete: Discrete.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Taxi driving.
+ * Observable: Partially.
+ * Agents: Multi.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Dynamic.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Medical diagnosis.
+ * Observable: Partially.
+ * Agents: Single.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Dynamic.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Medical diagnosis.
+ * Observable: Partially.
+ * Agents: Single.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Dynamic.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Image analysis.
+ * Observable: Fully.
+ * Agents: Single.
+ * Deterministic: Deterministic.
+ * Episodic: Episodic.
+ * Static: Semi.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Part-picking robot.
+ * Observable: Partially.
+ * Agents: Single.
+ * Deterministic: Stochastic.
+ * Episodic: Episodic.
+ * Static: Dynamic.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Refinery controller.
+ * Observable: Partially.
+ * Agents: Single.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Dynamic.
+ * Discrete: Continuous.
+ * -------------------------------------------------------------------------------------
+ * Task Environment: Interactive English tutor.
+ * Observable: Partially.
+ * Agents: Multi.
+ * Deterministic: Stochastic.
+ * Episodic: Sequential.
+ * Static: Dynamic.
+ * Discrete: Discrete.
+ * -------------------------------------------------------------------------------------
  *
  * @author Ciaran O'Reilly
  */
