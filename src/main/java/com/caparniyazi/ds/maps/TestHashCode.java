5af5849f431702931178c08439876c06d5348d78
caparniyazi <capar.niyazi.tr@gmail.com>
2025-10-12 18:29:27 +0300

    Custom HashTable implementation using Open Addressing.

--- src/main/java/com/caparniyazi/ds/maps/TestHashCode.java
+++ src/main/java/com/caparniyazi/ds/maps/TestHashCode.java
@@ -8,6 +8,12 @@ public class TestHashCode {
     }
 
     /**
+     * In mathematics and computer science, Horner's method (or Horner's scheme) is an
+     * algorithm for polynomial evaluation.
+     * Although named after William George Horner, this method is much older,
+     * as it has been attributed to Joseph-Louis Lagrange by Horner himself,
+     * and can be traced back many hundreds of years to Chinese and Persian(TÛSÎ, Nasîrüddin) mathematicians.
+     * <p/>
      * Method that returns the hashcode of the given string using the following
      * Java API algorithm:
      * * s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
