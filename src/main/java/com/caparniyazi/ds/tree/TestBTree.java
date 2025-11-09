60718f3de544e5ce4d17f7c30bc6a6249c23b22a
caparniyazi <capar.niyazi.tr@gmail.com>
2025-11-09 15:50:21 +0300

    B-Tree and B+Tree comparison.

--- src/main/java/com/caparniyazi/ds/tree/TestBTree.java
+++ src/main/java/com/caparniyazi/ds/tree/TestBTree.java
@@ -16,5 +16,17 @@ public class TestBTree {
         btree.remove(20);
         btree.printTree();
         System.out.println("Size: " + btree.getSize());
+        System.out.println("==========================================================");
+        System.out.println("");
+
+        btree = new BTree<>(5);
+        data = new int[]{20, 30, 8, 10, 15, 18, 44, 26, 28, 23, 25, 43, 55, 36, 44, 39};
+
+        for (int i : data) {
+            btree.add(i);
+        }
+        btree.printTree();
+        System.out.println("Size: " + btree.getSize());
+        System.out.println("==========================================================");
     }
 }
