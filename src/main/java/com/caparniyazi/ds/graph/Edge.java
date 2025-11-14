f196c78d39dae5415ab2771ee6cb6dfa9d3e63b6
caparniyazi <capar.niyazi.tr@gmail.com>
2025-11-14 12:30:50 +0300

    An Edge implementation for a Graph.

---
+++ src/main/java/com/caparniyazi/ds/graph/Edge.java
@@ -0,0 +1,67 @@
+package com.caparniyazi.ds.graph;
+
+import lombok.Getter;
+
+import java.util.Objects;
+
+/**
+ * An Edge represents a relationship between two vertices.
+ *
+ * @param <T>
+ */
+@Getter
+public class Edge<T extends Comparable<T>> {
+    // Data fields
+    private int source; // The source vertex for an edge.
+    private int dest;   // The destination vertex for an edge.
+    private double weight;   // the weight.
+
+    // Constructors
+    // Constructs an Edge from source to dest. Sets the weight to weight.
+    public Edge(int source, int dest, double weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    // Constructors
+    // Constructs an Edge from source to dest. Sets the weight to 1.0
+    public Edge(int source, int dest) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = 1.0;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) return false;
+
+        if (other == this) return true;
+        if (this.getClass() != other.getClass()) return false;
+
+        Edge<T> otherEdge = (Edge<T>) other;
+        if (this.source == otherEdge.source && this.dest == otherEdge.dest) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.source, this.dest);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[(");
+        sb.append(source);
+        sb.append(", ");
+        sb.append(dest);
+        sb.append("): ");
+        sb.append(weight);
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
